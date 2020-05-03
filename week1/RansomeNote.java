78a003c7700be38af6ffe95fcfb9758b355c7955
nitinigi2 <nitinsinghal139@gmail.com>
2020-05-03 16:57:08 +0530

    Added question link

--- week1/RansomeNote.java
+++ week1/RansomeNote.java
@@ -1,3 +1,5 @@
+Question link: https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3318/
+
 // method 1
 
 public boolean canConstruct(String ransomNote, String magazine) {
