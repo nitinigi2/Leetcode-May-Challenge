402be0e867efc2203872cad875d2a8e2a3ecec1c
nitinigi2 <nitinsinghal139@gmail.com>
2020-05-03 16:58:44 +0530

    Added question link

--- week/FirstBadVersion.java
+++ week/FirstBadVersion.java
@@ -1,3 +1,5 @@
+Question link: https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/
+
 public int firstBadVersion(int n) {
     int l = 1;
     int r = n;
