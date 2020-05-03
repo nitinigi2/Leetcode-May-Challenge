eacbd241a5c787b7b911699d77bfc1548525efbd
nitinigi2 <nitinsinghal139@gmail.com>
2020-05-03 17:01:41 +0530

    Added question link with solution

--- week1/JewelsAndStones.java
+++ week1/JewelsAndStones.java
@@ -1,3 +1,5 @@
+// Question link : https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317/    
+    
     public int numJewelsInStones(String J, String S) {
         HashMap<Character, Integer> map = new HashMap<>();
         int count=0;
